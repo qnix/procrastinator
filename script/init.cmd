b6ac2e55e36c040c5c4d250d52c63bc113a89c2e
Quan Ta <qta@paxata.com>
2016-08-28 13:20:39 -0700

    first commit

---
+++ script/init.cmd
@@ -0,0 +1,7 @@
+echo "# procrastinator" >> README.md
+git init
+git add README.md
+git add script
+git commit -m "first commit"
+git remote add origin git@github.com:qnix/procrastinator.git
+git push -u origin master
